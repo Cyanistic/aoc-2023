1ee179dc3b57cf3541e401beec1b7fd22f3cc80c
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2023-12-06 12:53:17 -0500

    extra day 6 solutions
    - added O(r) solution for part 1 and O(1) solution for part 2

--- src/day_6.rs
+++ src/day_6.rs
@@ -20,15 +20,26 @@ pub fn part_1() -> usize {
         .trim()
         .split_ascii_whitespace()
         .map(|d| d.parse().unwrap());
+    // O(r*t) solution
+    // r = # of races
+    // t = time
+    // time.zip(distance)
+    //     .map(|(t, d): (usize, usize)| (0..=t).filter(|i| i * (t - i) > d).count())
+    //     .product()
+    //
+    // O(r) solution
     time.zip(distance)
-        .map(|(t, d)| (0..=t).filter(|i| i * (t - i) > d).count())
+        .map(|(t, d): (f64, f64)| {
+            (((-t - (t * t - 4.0 * d).sqrt()) / (-2.0)).ceil() as usize - 1)
+                - (((-t + (t * t - 4.0 * d).sqrt()) / (-2.0)) as usize)
+        })
         .product()
 }
 
 pub fn part_2() -> usize {
     let contents = read_to_string("input/day-6.txt").unwrap();
     let (time, distance) = contents.split_once('\n').unwrap();
-    let time: usize = time
+    let time: f64 = time
         .split_once(':')
         .unwrap()
         .1
@@ -36,7 +47,7 @@ pub fn part_2() -> usize {
         .replace(' ', "")
         .parse()
         .unwrap();
-    let distance = distance
+    let distance: f64 = distance
         .split_once(':')
         .unwrap()
         .1
@@ -44,5 +55,9 @@ pub fn part_2() -> usize {
         .replace(' ', "")
         .parse()
         .unwrap();
-    (0..=time).filter(|i| i * (time - i) > distance).count()
+    // O(t) solution
+    // (0..=time).filter(|i| i * (time - i) > distance).count()
+    // O(1) solution
+    (((-time - (time * time - 4.0 * distance).sqrt()) / (-2.0)).ceil() as usize - 1)
+        - (((-time + (time * time - 4.0 * distance).sqrt()) / (-2.0)) as usize)
 }
